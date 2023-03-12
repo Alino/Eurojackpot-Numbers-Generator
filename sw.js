f1cfa9d147bbe757f9f543ae32c051426760fbbc
Alexander Sadovsky <alino.sadovsky@gmail.com>
2023-03-13 00:25:57 +0100

    add service worker for caching in offline mode

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/bootstrap.min.css',
+  '/jquery-3.5.1.slim.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
